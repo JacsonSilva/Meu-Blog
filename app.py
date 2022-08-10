32f3eb4122a6d58c53c67e7f1acf347b880916d2
JACSON G. DA SILVA <jacson.analistaderede@gmail.com>
2022-08-10 17:14:51 +0000

    Adcionando index.html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/pagina1')
-def hello():
-    return "<h1>Meu Primeiro Site em Python</h1>"
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 @app.route("/contatos")
 def contato():
